f1980af6b568ec36f4e658054f058a4eb572efbb
Zakari Badiru <zbadiru@live.co.uk>
2020-04-08 21:06:06 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -19,7 +19,7 @@ def get_user_input
 end
 
 def end_game(card_total)
-  puts "Sorry, you hit#{card_total}. Thanks for playing!"
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
