195f00b6fbdc72982280762a663f75880b70ef85
Zakari Badiru <zbadiru@live.co.uk>
2020-04-08 21:11:35 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -53,12 +53,12 @@ end
 #####################################################
 
 def runner
-#   Welcome
-#   card_total = initial_round
-#   until card_total > 21
-#   card_total = hit? (card_total)
-#   display_card_total(card_total)
-# end 
-# end_game(card_total)
+  Welcome
+  card_total = initial_round
+  until card_total > 21
+  card_total = hit? (card_total)
+  display_card_total(card_total)
+end 
+end_game(card_total)
 end
     
